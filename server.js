e6665d47712b5ea01743c003a7f8da859ba31546
srhdp <srhdp@MacBook-Pro-2.local>
2025-04-25 14:11:01 +0700

    cart api

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const RatingRoute = require("./routes/rating")
 const AuthRoute = require("./routes/auth");
 const UserRoute = require("./routes/user");
 const AddressRoute = require("./routes/address");
+const CartRoute = require("./routes/cart");
 
 dotenv.config();
 
@@ -26,5 +27,6 @@ app.use("/api/restaurant", RestaurantRoute);
 app.use("/api/foods", FoodRoute);
 app.use("/api/rating", RatingRoute);
 app.use("/api/address", AddressRoute);
+app.use("/api/cart", CartRoute);
 
 app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is running on ${process.env.PORT}!`))
